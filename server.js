a90ab984560278134f4f7e882a3f7f91fd995e5f
2022project <ghkdsigm3@gmail.com>
2022-03-15 01:19:15 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+
+const path = require('path')
+
+const router = jsonServer.router(path.join(__dirname,'db.json'))
+
+const middlewares = jsonServer.defaults()
+
+const customMiddlewares = require(path.join(__dirname, 'auth-middleware.js'))
+
+const port = process.env.PORT || 5000;
+
+
+// 미들웨어 설정 
+server.use(middlewares)
+server.use(customMiddlewares)
+
+const customRouter = jsonServer.rewriter({
+  "/api/v2/*": "/$1",
+  "/:resource/:id/show": "/:resource/:id",
+  "/posts/:category": "/posts?category=:category",
+  "/articles\\?id=:id": "/posts/:id",
+  "/findUser\\?name=:name": "/users/?name=:name"
+})
+// 커스텀 라우터 설정 
+server.use(customRouter);
+
+
+// json-server 라우트 
+server.use(router)
+
+server.listen(port, () => {
+  console.log('JSON Server is running')
+})
