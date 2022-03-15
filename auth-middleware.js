c0535de9fe1255fa75bd2214231967e6f8bffa16
2022project <ghkdsigm3@gmail.com>
2022-03-15 11:55:13 +0900

    make it better

--- auth-middleware.js
+++ auth-middleware.js
@@ -1,15 +1,15 @@
 module.exports = (req, res, next) => {
     // res.header('X-Hello', 'World')
     next()
-    if (req.path == '/users') {
-      next()
-    } else {
-      res.status(400).json({ err: "요청하신 정보가 존재하지않습니다." });
-      res.status(401).json({ err: "인증에실패했습니다." });
-      res.status(403).json({ err: "허용되지않습니다." });
-      res.status(404).json({ err: "페이지가 존재하지않습니다." });
-      res.status(429).json({ err: "서버가 초과했습니다." });
-      res.status(500).json({ err: "서버가 오류가발생했습니다." });
-    }
+    // if (req.path == '/users') {
+    //   next()
+    // } else {
+    //   res.status(400).json({ err: "요청하신 정보가 존재하지않습니다." });
+    //   res.status(401).json({ err: "인증에실패했습니다." });
+    //   res.status(403).json({ err: "허용되지않습니다." });
+    //   res.status(404).json({ err: "페이지가 존재하지않습니다." });
+    //   res.status(429).json({ err: "서버가 초과했습니다." });
+    //   res.status(500).json({ err: "서버가 오류가발생했습니다." });
+    // }
   
   }
